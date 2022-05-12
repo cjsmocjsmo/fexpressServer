6a4e0279ae22e89b64f64a1d7395e2efecdf79cf
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-05-11 22:46:24 -0700

    rm docker use a venv

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
-python3 -m venv /home/teresa/fexpressServer;
-cd /home/teresa/fexpressServer;
+#!/bin/sh
+cd /home/teresa;
+git clone https://github.com/cjsmocjsmo/fexpress;
+cd /home/teresa/fsexpress
+
+python3 -m venv /home/teresa/fexpress/fexpressServer;
+cd /home/teresa/fexpress/fexpressServer;
+mv /home/teresa/fexpress/fexpressServer.py /home/teresa/fexpress/fsexpressServer/fexpressServer.py
+mv /home/teresa/fexpress/util.py /home/teresa/fexpress/fsexpressServer/util.py
+
 sudo apt-get update && \
 sudo apt-get dist-upgrade -y && \
 sudo apt-get autoclean -y && \
@@ -9,5 +17,5 @@ sudo apt-get install --no-install-recommends -y \
 	python3-dev \
     python3-setuptools \
 	python3-tornado;
-git clone https://github.com/cjsmocjsmo/fexpress;
-python3 /home/teresa/fexpressServer/fexpress/fexpressServer.py
+
+python3 /home/teresa/fexpressServer/fexpress/fexpressServer.py;
